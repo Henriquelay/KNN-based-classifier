37fd51e2511c87746d698c8de01c5589b1f8a8d6
Henriquelay <henriquelayber@gmail.com>
2018-10-25 00:21:35 -0300

    Euclides done

--- distancias.c
+++ distancias.c
@@ -8,25 +8,40 @@
 #include <stdio.h>
 #include <math.h>
 
-//linhas e colunas de treinamento mais tarde transformar em variavel global
-//deste cara (featureTarget), use isso como base (featureTraining), 
-//a resposta será a distância entre o target e o treinamento OS OUTROS SAO INFORMAÇÕES NECESSARIAS, REMOVER FUTURAMENTE
+//Início do bloco Euclides
 float euclidiana(float featureTarget, float featureTraining){
     float resultado = sqrt((featureTarget - featureTraining) * (featureTarget - featureTraining));
     return resultado;
 }
 
 //implementa a distância euclidiana já como vetor
-float* euclidianaVetor(float* featureTarget, float* featureTraining){
-    float* resultado = (float*) malloc(sizeof(featureTarget);
+float* euclidianaVetor(float* linhaTarget, float* linhaTraining){
+    float* resultado = (float*) malloc(sizeof(linhaTarget));
 
-    if(sizeof(featureTarget )!= sizeof(featureTraining)){
+    if(sizeof(linhaTarget)!= sizeof(linhaTraining)){
         printf("\n\n\tFUDEU GERAL, O ARGUMENTO TEM NUMERO DE FEATURES DIFERENTE DO TREINAMENT0\n");
-        return NULL;
+        exit(1);    //TODO: dar os frees antes de encerrar
     }
 
-    for(int i = 0; i < sizeof(featureTarget), i++)
-        resultado[i] = euclidiana(featureTarget[i], featureTraining[i]);
+    for(int i = 0; i < sizeof(linhaTarget); i++)
+        resultado[i] = euclidiana(linhaTarget[i], linhaTraining[i]);
     
+    return resultado;
+}
+
+//implementa a distância euclidiana como vetor, como linhas de umas matriz
+float** euclidianaMatriz(float** matrizTarget, float** matrizTraining){
+    float** resultado = (float*) malloc(sizeof(matrizTarget));
+
+    /*analizar se esse IF é de fato necessário 
+    (se entradas de linhas diferentes devem ser processadas, e se sim, de que forma)*/
+    if(sizeof(matrizTarget)!= sizeof(matrizTraining)){
+        printf("\n\n\tFUDEU GERAL, O ARGUMENTO TEM NUMERO DE ENTRADAS DIFERENTE DO TREINAMENT0\n");
+        exit(1);    //TODO: dar os frees antes de encerrar
+    }
+
+    for(int i = 0; i < sizeof(matrizTarget); i++)
+        resultado[i] = euclidianaVetor(matrizTarget[i], matrizTraining[i]);
+
     return resultado;
 }
