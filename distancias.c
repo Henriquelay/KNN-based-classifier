c15bf74a5733067eaf72560eec607750674052a0
Henriquelay <henriquelayber@gmail.com>
2018-10-25 00:59:31 -0300

    Minkowski feito, Euclides corrigido, um passo mais próximo da estrelinha

--- distancias.c
+++ distancias.c
@@ -10,28 +10,31 @@
 
 //Início do bloco Euclides
 float euclidiana(float featureTarget, float featureTraining){
-    float resultado = sqrt((featureTarget - featureTraining) * (featureTarget - featureTraining));
+    float resultado = (featureTarget - featureTraining) * (featureTarget - featureTraining);
     return resultado;
 }
 
-//implementa a distância euclidiana já como vetor
-float* euclidianaVetor(float* linhaTarget, float* linhaTraining){
-    float* resultado = (float*) malloc(sizeof(linhaTarget));
+//implementa a distância euclidiana sobre um vetor
+float euclidianaVetor(float* linhaTarget, float* linhaTraining){
+    int i;
+    float acc = 0;
 
     if(sizeof(linhaTarget)!= sizeof(linhaTraining)){
         printf("\n\n\tFUDEU GERAL, O ARGUMENTO TEM NUMERO DE FEATURES DIFERENTE DO TREINAMENT0\n");
-        exit(1);    //TODO: dar os frees antes de encerrar
+        exit(1);
     }
 
-    for(int i = 0; i < sizeof(linhaTarget); i++)
-        resultado[i] = euclidiana(linhaTarget[i], linhaTraining[i]);
+    for(i = 0; i < sizeof(linhaTarget); i++)
+        acc += euclidiana(linhaTarget[i], linhaTraining[i]);
     
+    float resultado = sqrt(acc);
+
     return resultado;
 }
 
-//implementa a distância euclidiana como vetor, como linhas de umas matriz
-float** euclidianaMatriz(float** matrizTarget, float** matrizTraining){
-    float** resultado = (float*) malloc(sizeof(matrizTarget));
+//implementa um vetor de euclidianaVetor
+float* euclidianaMatrix(float** matrizTarget, float** matrizTraining){
+    float* resultado = (float*) malloc(sizeof(matrizTarget));
 
     /*analizar se esse IF é de fato necessário 
     (se entradas de linhas diferentes devem ser processadas, e se sim, de que forma)*/
@@ -44,4 +47,44 @@ float** euclidianaMatriz(float** matrizTarget, float** matrizTraining){
         resultado[i] = euclidianaVetor(matrizTarget[i], matrizTraining[i]);
 
     return resultado;
+}
+
+
+//Início do bloco Minkowski
+float minkowski(float featureTarget, float featureTraining, float r){
+    float resultado = pow(abs(featureTarget - featureTraining), r);
+    return resultado;
+}
+
+//implementa Minkowski sobre um vetor
+float minkowskiVetor(float* linhaTarget, float* linhaTraining, float r){
+    int i;
+    float acc = 0;
+
+    if(sizeof(linhaTarget)!= sizeof(linhaTraining)){
+        printf("\n\n\tFUDEU GERAL, O ARGUMENTO TEM NUMERO DE FEATURES DIFERENTE DO TREINAMENT0\n");
+        exit(1);    //TODO: dar os frees antes de encerrar
+    }
+
+    for(i = 0; i < sizeof(linhaTarget); i++)
+        acc += minkowski(linhaTarget[i], linhaTraining[i], r);
+
+    float resultado = pow(acc, 1/r);
+
+    return resultado;
+}
+
+//implementa um vetor de minkowskiVetor
+float* minkowskiMatrix(float** matrizTarget, float** matrizTraining, float r){
+    float* resultado = (float*) malloc(sizeof(matrizTarget));
+
+    /*analizar se esse IF é de fato necessário 
+    (se entradas de linhas diferentes devem ser processadas, e se sim, de que forma)*/
+    if(sizeof(matrizTarget)!= sizeof(matrizTraining)){
+        printf("\n\n\tFUDEU GERAL, O ARGUMENTO TEM NUMERO DE ENTRADAS DIFERENTE DO TREINAMENT0\n");
+        exit(1);    //TODO: dar os frees antes de encerrar
+    }
+
+    for(int i = 0; i < sizeof(matrizTarget); i++)
+        resultado[i] = minkowskiVetor(matrizTarget[i], matrizTraining[i], r);
 }
