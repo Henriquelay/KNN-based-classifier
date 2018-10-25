08125cf6c58d0e2e24909c275a33224937c706f6
Henriquelay <henriquelayber@gmail.com>
2018-10-25 01:16:53 -0300

    Similaridade de Cossenos (ESTRELINHA AQUI VOU EU)

--- distancias.c
+++ distancias.c
@@ -9,31 +9,31 @@
 #include <math.h>
 
 //Início do bloco Euclides
-float euclidiana(float featureTarget, float featureTraining){
+float euclides(float featureTarget, float featureTraining){
     float resultado = (featureTarget - featureTraining) * (featureTarget - featureTraining);
     return resultado;
 }
 
 //implementa a distância euclidiana sobre um vetor
-float euclidianaVetor(float* linhaTarget, float* linhaTraining){
+float euclidesVetor(float* vetorTarget, float* vetorTraining){
     int i;
     float acc = 0;
 
-    if(sizeof(linhaTarget)!= sizeof(linhaTraining)){
+    if(sizeof(vetorTarget)!= sizeof(vetorTraining)){
         printf("\n\n\tFUDEU GERAL, O ARGUMENTO TEM NUMERO DE FEATURES DIFERENTE DO TREINAMENT0\n");
         exit(1);
     }
 
-    for(i = 0; i < sizeof(linhaTarget); i++)
-        acc += euclidiana(linhaTarget[i], linhaTraining[i]);
+    for(i = 0; i < sizeof(vetorTarget); i++)
+        acc += euclides(vetorTarget[i], vetorTraining[i]);
     
     float resultado = sqrt(acc);
 
     return resultado;
 }
 
-//implementa um vetor de euclidianaVetor
-float* euclidianaMatrix(float** matrizTarget, float** matrizTraining){
+//implementa um vetor de euclidesVetor
+float* euclidesMatrix(float** matrizTarget, float** matrizTraining){
     float* resultado = (float*) malloc(sizeof(matrizTarget));
 
     /*analizar se esse IF é de fato necessário 
@@ -44,7 +44,7 @@ float* euclidianaMatrix(float** matrizTarget, float** matrizTraining){
     }
 
     for(int i = 0; i < sizeof(matrizTarget); i++)
-        resultado[i] = euclidianaVetor(matrizTarget[i], matrizTraining[i]);
+        resultado[i] = euclidesVetor(matrizTarget[i], matrizTraining[i]);
 
     return resultado;
 }
@@ -57,17 +57,17 @@ float minkowski(float featureTarget, float featureTraining, float r){
 }
 
 //implementa Minkowski sobre um vetor
-float minkowskiVetor(float* linhaTarget, float* linhaTraining, float r){
+float minkowskiVetor(float* vetorTarget, float* vetorTraining, float r){
     int i;
     float acc = 0;
 
-    if(sizeof(linhaTarget)!= sizeof(linhaTraining)){
+    if(sizeof(vetorTarget)!= sizeof(vetorTraining)){
         printf("\n\n\tFUDEU GERAL, O ARGUMENTO TEM NUMERO DE FEATURES DIFERENTE DO TREINAMENT0\n");
         exit(1);    //TODO: dar os frees antes de encerrar
     }
 
-    for(i = 0; i < sizeof(linhaTarget); i++)
-        acc += minkowski(linhaTarget[i], linhaTraining[i], r);
+    for(i = 0; i < sizeof(vetorTarget); i++)
+        acc += minkowski(vetorTarget[i], vetorTraining[i], r);
 
     float resultado = pow(acc, 1/r);
 
@@ -87,4 +87,27 @@ float* minkowskiMatrix(float** matrizTarget, float** matrizTraining, float r){
 
     for(int i = 0; i < sizeof(matrizTarget); i++)
         resultado[i] = minkowskiVetor(matrizTarget[i], matrizTraining[i], r);
+}
+
+
+//Dependencia para a similaridadeCosseno
+float produtoInterno(float* vetorA, float* vetorB){
+    int i;
+    float resultado = 0;
+    
+    if(sizeof(vetorA) != sizeof(vetorB)){
+        printf("\n\n\tFUDEU GERAL, ESSES VETORES NÃO TEM PRODUTO INTERNO (TAMANHOS DIFERENTES!!): %i : %i\n", vetorA, vetorB);
+        exit(1);
+    }
+
+    for(i = 0; i < sizeof(vetorA); i++)
+        resultado += vetorA[i] * vetorB[i];
+
+    return resultado;
+}
+
+//Início do bloco similaridade de cosseno
+float similaridadeCosseno(float* vetorTarget, float* vetorTraining){
+    float resultado = produtoInterno(vetorTarget, vetorTraining) / euclidesVetor(vetorTarget, vetorTraining);
+    return resultado;
 }
