4cbf823adccedcd57e65e8a6d388323419766eb0
HenriqueLay <henriquelayber@gmail.com>
2018-11-01 17:05:51 -0200

    metadeReadConfig (estrelinha ficando longe)

--- filemanager.c
+++ filemanager.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void textSize(FILE *arquivo, int *colunas, int *linhas){
     char *texto;
@@ -25,9 +26,9 @@ void textSize(FILE *arquivo, int *colunas, int *linhas){
     *linhas = l;
 }
 
-void printaMatriz(float **matriz, float *iD, int Ncolunas, int Nlinhas){
-    for(int i = 0;  i < Nlinhas; i++){
-        for(int j = 0; j < Ncolunas; j++){
+void printaMatriz(float **matriz, float *iD, int nColunas, int nLinhas){
+    for(int i = 0;  i < nLinhas; i++){
+        for(int j = 0; j < nColunas; j++){
             printf("%.2f ", matriz[i][j]); 
         }
         printf("%.0f\n", iD[i]);
@@ -46,7 +47,56 @@ void transcribe(FILE *arquivo, float **matrizAmostra, float *rotuloVet, int colu
     rewind(arquivo);
 }
 
+
+
+void openConfig(FILE *configuracao, char *pathTreino, char *pathTeste, char* pathPredicao, int *k, char *distancia, float *coefMinkowski, int *linhas){
+
+    k = (int*) malloc(nLinhas * sizeof(int));
+    distancia = (char*) malloc(nLinhas * sizeof(char));
+    coefMinkowski = (float*) malloc(nLinhas * sizeof(float));
+
+    for(int quebraLinhas = 0; quebraLinhas < 4; quebraLinhas++){
+        fscanf(configuracao, "%s", pathTreino);
+        fscanf(configuracao, "%s", pathTeste);
+        fscanf(configuracao, "%s", pathPredicao);
+    }
+
+    int nLinhas = 0;
+    for(int i = 0; !feof(configuracao); i++){
+        while(configuracao != "\n"){ 
+            fscanf(configuracao, "%i, %c", &k[i], &distancia[i]);
+            if(k[i] == 'M')
+                fscanf(configuracao, "%f", &coefMinkowski[i]);
+            else
+                coefMinkowski[i] = NULL;
+        }
+        nLinhas++;
+    }
+
+    *linhas = nLinhas;
+    rewind(configuracao);
+}
+
 void main(){
+    FILE *config = fopen("iris/config.txt", "r");
+    int *k, nLinhas;
+    char *tipoDistancia, *pathTreino, *pathTeste, *pathPredicao;
+    float *coefMinkowski;
+
+    openConfig(config, pathTreino, pathTeste, pathPredicao, k, tipoDistancia, coefMinkowski, &nLinhas);
+
+// printa os vetores
+    printf("Path Treino = %s\nPathTeste = %s\nPathPredicao = %s", pathTreino, pathTeste, pathPredicao);
+    for(int i = 0; i < sizeof(k); i++){
+        printf("K[%i] = %i\nDist[%i] = %c\ncoefMinkowski[%i] = %f\n\n", i, k[i], i, tipoDistancia[i], i, coefMinkowski[i]);
+    }
+
+    free(k);
+    free(coefMinkowski);
+    fclose(config);
+
+
+    /*
     FILE *arq = fopen("vowels/dataset/vowels_teste.csv", "r");
     float **amostras;
     float *rotulo;
@@ -77,4 +127,5 @@ void main(){
     free(rotulo);
     fclose(arq);
     printf("\n\n");
+    */
 }
