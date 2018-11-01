4ba4c76c09144ab1d9a44baa228368039abb0e91
Davi-Manjaro <davispetris@gmail.com>
2018-11-01 01:07:12 -0300

    file manager rumo a estrelinha

--- filemanager.c
+++ filemanager.c
@@ -3,23 +3,33 @@
 
 void main(){
     FILE *arq = fopen("iris/dataset/iris_treino.csv", "r");
-    char *teste;
+    float *teste;
+    int *c;
 
     if (arq==NULL){
         printf("DEU MERDA");
         exit(1);
     }
 
-    teste = malloc(100 * sizeof(char));
+    //Colocando todos os valores em um único ponteiro
+    teste = (float *)malloc(sizeof(float));
+    c = (int *)malloc(sizeof(int));
 
-    for(int i = 0; !feof(arq); i++){
-        fscanf(arq, "%c", &teste[i]);
-        teste = realloc(teste, (i+2)*sizeof(char));
+    for(c[0] = 0; !feof(arq); c[0]++){
+        fscanf(arq, "%f,", &teste[c[0]]);
+        // printf("%d\n", i);
+        teste = realloc(teste, (c[0]+2)*sizeof(float));
     }
+    c[0] = c[0] - 1;
+    printf("%d", c[0]);
     
-    printf("%s", teste);
+    for(int i = 0; i<c[0]; i++){
+        printf("%.2f ", teste[i]);
+    }
     free(teste);
 
     fclose(arq);
-    
+
+    //Fazer uma função que coloque os valores de cada linha (ponteiro) em outro ponteiro (matriz
+
 }
