48972ccf0467a31f7a76ea01148a35e44de3a311
DaviPtrs <davispetris@gmail.com>
2018-11-09 22:47:11 -0200

    knn.dist implementada

--- main.c
+++ main.c
@@ -3,6 +3,44 @@
 #include "distancias.h"
 #include "openfile.h"
 
+struct distRot{ 
+        float dist;
+        float rotulo;
+    };
+
+typedef struct distRot kneigh;
+
+
+void knnDist(kneigh ***MatrizNeighs, float **matrizTreino, float *rotuloTreino, float **matrizTeste, 
+    char tipoDist, float coefMinkowski, int linhasTreino, int colunasTreino, int linhasTeste, int colunasTeste){
+
+    kneigh **vizinho;
+
+    vizinho = (kneigh **) malloc(linhasTeste * sizeof(kneigh*));
+    for(int i = 0; i < linhasTeste; i++){
+        vizinho[i] = (kneigh *) malloc(linhasTreino * sizeof(kneigh));
+    }
+
+    // //CALCULO DE DISTÂNCIA
+    for(int aTst = 0; aTst < linhasTeste; aTst++){
+        for(int aTrn = 0; aTrn < linhasTreino; aTrn ++){
+            switch(tipoDist){
+                case 'E':
+                    vizinho[aTst][aTrn].dist = euclidesVetor(matrizTeste[aTst],colunasTeste, matrizTreino[aTrn], colunasTreino);
+                    break;
+                case 'C':
+                    vizinho[aTst][aTrn].dist = chebyshevVetor(matrizTeste[aTst],colunasTeste, matrizTreino[aTrn], colunasTreino);
+                    break;
+                case 'M':
+                    vizinho[aTst][aTrn].dist = minkowskiVetor(matrizTeste[aTst],colunasTeste, matrizTreino[aTrn], colunasTreino, coefMinkowski);
+            }
+            vizinho[aTst][aTrn].rotulo = rotuloTreino[aTrn];
+        }
+    }
+    *MatrizNeighs = vizinho;
+}
+
+
 void main(){
     FILE *config = fopen("iris/config.txt", "r");
     int *k, linhasConfig;
@@ -12,16 +50,16 @@ void main(){
 
     setupConfig(config, &pathTreino, &pathTeste, &pathPredicoes, &k, &tipoDistancia, &coefMinkowski, &linhasConfig);
 
-    puts("PATHS:");
-    printf("%s\n", pathTreino);
-    printf("%s\n", pathTeste);
-    printf("%s\n", pathPredicoes);
+    // puts("PATHS:");
+    // printf("%s\n", pathTreino);
+    // printf("%s\n", pathTeste);
+    // printf("%s\n", pathPredicoes);
 
     
-    puts("\nCONFIGS:");
-    for(int i = 0; i < linhasConfig - 3; i++){
-        printf("%d %c %.2f\n", k[i], tipoDistancia[i], coefMinkowski[i]);
-    }
+    // puts("\nCONFIGS:");
+    // for(int i = 0; i < linhasConfig - 3; i++){
+    //     printf("%d %c %.2f\n", k[i], tipoDistancia[i], coefMinkowski[i]);
+    // }
 
 
     FILE *treino = fopen(pathTreino, "r");
@@ -35,8 +73,8 @@ void main(){
     
     transcribe(treino, &matrizTreino, &rotuloTreino, &linhasTreino, &colunasTreino);
 
-    puts("\nTREINO:");
-    printaMatriz(matrizTreino, rotuloTreino, colunasTreino, linhasTreino);
+    // puts("\nTREINO:");
+    // printaMatriz(matrizTreino, rotuloTreino, colunasTreino, linhasTreino);
 
     FILE *teste = fopen(pathTeste, "r");
     float **matrizTeste, *rotuloTeste;
@@ -49,48 +87,18 @@ void main(){
     
     transcribe(teste, &matrizTeste, &rotuloTeste, &linhasTeste, &colunasTeste);
 
-    puts("\nTESTE:");
-    printaMatriz(matrizTeste, rotuloTeste, colunasTeste, linhasTeste);
+    // puts("\nTESTE:");
+    // printaMatriz(matrizTeste, rotuloTeste, colunasTeste, linhasTeste);
 
     // ///COMEÇO DO KNN
 
-    struct distRot{ 
-        float dist;
-        float rotulo;
-    };
+    kneigh **matrizVizinhos;
 
-    typedef struct distRot kneigh;
-
-    kneigh **vizinho;
-
-    vizinho = (kneigh **) malloc(linhasTeste * sizeof(kneigh*));
-    for(int i = 0; i < linhasTeste; i++){
-        vizinho[i] = (kneigh *) malloc(linhasTreino * sizeof(kneigh));
-    }
-
-    // void knnDist(kneigh ***vizinho, char tipoDist, float coefMinkowski, int linhasTreino, int colunasTreino, int linhasTeste, int colunasTeste);
-
-    // //CALCULO DE DISTÂNCIA
-    for(int aTst = 0; aTst < linhasTeste; aTst++){
-        for(int aTrn = 0; aTrn < linhasTreino; aTrn ++){
-            switch(tipoDistancia[2]){
-                case 'E':
-                    vizinho[aTst][aTrn].dist = euclidesVetor(matrizTeste[aTst],colunasTeste, matrizTreino[aTrn], colunasTreino);
-                    break;
-                case 'C':
-                    vizinho[aTst][aTrn].dist = chebyshevVetor(matrizTeste[aTst],colunasTeste, matrizTreino[aTrn], colunasTreino);
-                    break;
-                case 'M':
-                    vizinho[aTst][aTrn].dist = minkowskiVetor(matrizTeste[aTst],colunasTeste, matrizTreino[aTrn], colunasTreino, coefMinkowski[1]);
-            }
-            vizinho[aTst][aTrn].rotulo = rotuloTreino[aTrn];
-        }
-    }
-
+    knnDist(&matrizVizinhos, matrizTreino, rotuloTreino, matrizTeste, tipoDistancia[2], coefMinkowski[2], linhasTreino, colunasTreino, linhasTeste, colunasTeste);
 
     for(int j = 0; j < linhasTreino; j++){
         for(int i = 0;  i < linhasTeste; i++){
-            printf("%f ", ((vizinho[i][j]).dist)); 
+            printf("%.f ", ((matrizVizinhos[i][j]).dist)); 
         }
         puts("");
     }
@@ -108,6 +116,6 @@ void main(){
     free(rotuloTreino);
     free(matrizTeste);
     free(rotuloTeste);
-    free(vizinho);
+    free(matrizVizinhos);
     puts("");
 }
