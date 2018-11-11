f36a16a786d07453777a66fb88b831568d407a20
DaviPtrs <davispetris@gmail.com>
2018-11-11 04:05:51 -0200

    knn funcionando pros configs

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "distancias.h"
 #include "filemanager.h"
+#include <string.h>
 #include "knn.h"
+
 
 int main(int argc, char *argv[]){
     FILE *config = fopen(argv[1], "r");
@@ -35,43 +38,51 @@ int main(int argc, char *argv[]){
     
     transcribe(testeFile, &teste.matriz, &teste.rotulo, &teste.nlinhas, &teste.ncolunas);
 
-   
+    for(int c = 0; c < nLinhasVetores; c++){
+        float *vetorClassificados;
+        float maiorRotulo;
 
-    float *vetorClassificados;
-    float maiorRotulo;
+        knn(&vetorClassificados, &maiorRotulo, treino, teste, amostras[c]);
+        
+        int **matrizConfusa;
+        matrizConfusa = (int **) calloc(maiorRotulo+1, sizeof(int *));
 
-    int c = 2;
-    knn(&vetorClassificados, &maiorRotulo, treino, teste, amostras[c]);
-    
-    int **matrizConfusa;
-    matrizConfusa = (int **) calloc(maiorRotulo+1, sizeof(int *));
+        for(int i = 0; i <= maiorRotulo; i++){
+            matrizConfusa[i] = (int *) calloc(maiorRotulo+1, sizeof(int));
+        }
 
-    for(int i = 0; i <= maiorRotulo; i++){
-        matrizConfusa[i] = (int *) calloc(maiorRotulo+1, sizeof(int));
+        int acertos = 0;
+        for(int i = 0; i < teste.nlinhas; i++){
+            if(vetorClassificados[i] == teste.rotulo[i])
+                acertos++;
+            matrizConfusa[(int)teste.rotulo[i]][(int)vetorClassificados[i]]++;
+        }
+
+        float acc = (float)acertos / (float)teste.nlinhas;
+
+        char *indice;
+        indice = (char *) malloc(nLinhasVetores * sizeof(char));
+        sprintf(indice, "%d", c+1);
+        indice = (char *) realloc(indice, strlen(indice) * sizeof(char));
+        
+        char saida[60] = "predicao_";
+
+        strcat(saida, indice);
+        strcat(saida, ".txt");
+
+        FILE* arq = fopen(saida, "w");
+
+        fprintf(arq, "%.2f\n", acc); //PRINTA ACCURACY
+
+        printMatrizFile(arq, (int)maiorRotulo+1, matrizConfusa);
+
+        printVetorFile(arq, teste.nlinhas, vetorClassificados);
+        fclose(arq);
+        free(matrizConfusa);
+        free(vetorClassificados);
+        free(indice);
     }
 
-    int acertos = 0;
-    for(int i = 0; i < teste.nlinhas; i++){
-        if(vetorClassificados[i] == teste.rotulo[i])
-            acertos++;
-        matrizConfusa[(int)teste.rotulo[i]][(int)vetorClassificados[i]]++;
-    }
-
-    float acc = (float)acertos / (float)teste.nlinhas;
-
-    
-
-    FILE* arq = fopen("saida.txt", "w");
-
-
-    fprintf(arq, "%.2f\n", acc); //PRINTA ACCURACY
-
-    printMatrizFile(arq, (int)maiorRotulo+1, matrizConfusa);
-
-    printVetorFile(arq, teste.nlinhas, vetorClassificados);
-
-    fclose(arq);
-    free(matrizConfusa);
     free(amostras);
     free(paths->pathTreino);
     free(paths->pathTeste);
@@ -82,8 +93,6 @@ int main(int argc, char *argv[]){
     free(treino.rotulo);
     free(teste.matriz);
     free(teste.rotulo);
-    // free(matrizVizinhos);
-    free(vetorClassificados);
     puts("");
 
     return 0;
