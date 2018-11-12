ceb76953cfd251d2975fe076cc5d2eb81b760bae
HenriqueLay <henriquelayber@gmail.com>
2018-11-12 18:51:54 -0200

    aaaaaaaa

--- main.c
+++ main.c
@@ -6,52 +6,6 @@
 #include "headers/knn.h"
 #include "headers/aux.h"
 
-void output(char **pathPredicao, int *c, float *acc, float **vetorClassificados, int  ***matrizConfusa, char **jordana, float *maiorRotulo, int *nlinhas){
-
-        sprintf(*jordana, "%s%s%i%s", *pathPredicao ,"predicao_", *c + 1, ".txt");
-        printf(">Arquivo de saída: %s\n", *jordana);
-
-        FILE *predicao = fopen(*jordana, "w");
-
-        if (predicao == NULL){
-            printf("Erro na gravação dos resultados\n");
-            printf("Pode ser que o diretório %s não exista.\n", *pathPredicao);
-            exit(1);
-        }
-
-        fprintf(predicao, "%.2f\n", *acc); //PRINTA ACCURACY
-        printMatrizFile(predicao, (int) *maiorRotulo + 1, *matrizConfusa);
-        printVetorFile(predicao, *nlinhas, *vetorClassificados);
-
-        fclose(predicao);
-}
-
-float geraConfusao(int ***matrizConfusa, Data *teste, float **vetorClassificados, float *maiorRotulo){
-    int **matrizPerdida = (int**) calloc(*maiorRotulo + 1, sizeof(int*));
-    //calloc pois fazemos a confusão por incrementos
-    for(int i = 0; i < *maiorRotulo + 1; i++){
-        matrizPerdida[i] = (int*) calloc(*maiorRotulo + 1, sizeof(int));
-    }
-
-    int acertos = 0;
-    for(int i = 0; i < teste->nlinhas; i++){
-        if((*vetorClassificados)[i] == teste->rotulo[i]) acertos++;
-        matrizPerdida[(int) teste->rotulo[i]][(int) (*vetorClassificados)[i]]++;
-    }
-    *matrizConfusa = matrizPerdida;
-    return ((float) acertos / (float) teste->nlinhas);
-}
-
-int contaDigito(int num){
-    int nDigitos = 1;
-    while(num > 9){
-        num = num % 10;
-        nDigitos++;
-    }
-    return nDigitos;
-}
-
-
 int main(int argc, char *argv[]){
     FILE *config = fopen(argv[1], "r");
 
